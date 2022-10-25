4c9fbf9bf39cc2325b580bb642af2e8a4d410f2f
mmillet88 <m.kosyuchenko@gmail.com>
2022-10-25 18:18:16 +0100

    Checks whether the guess is in the wword wihtout using Milestone2.

--- milestone_3.py
+++ milestone_3.py
@@ -1,5 +1,5 @@
 import random
-import milestone_2
+# import milestone_2
 
 while True:
     guess = input("Please guess a letter")
@@ -8,7 +8,15 @@ while True:
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
 
-if guess in milestone_2.word:
+# if guess in milestone_2.word:
+#     print(f"Good guess! {guess} is in the word.")
+# else:
+#     print(f"Sorry, {guess} is not in the word. Try again.")
+
+word_list = ['apple', 'banana', 'cherries', "strawberries", "mango"]
+word = random.choice(word_list)
+
+if guess in word:
     print(f"Good guess! {guess} is in the word.")
 else:
     print(f"Sorry, {guess} is not in the word. Try again.")
