3ffbbe2e86551c76e5ba86c887e4e44c45c73c3d
mmillet88 <m.kosyuchenko@gmail.com>
2022-10-25 17:58:59 +0100

    Checks an input is a letter (corrected).

--- milestone_2.py
+++ milestone_2.py
@@ -4,7 +4,7 @@ print(word_list)
 word = random.choice(word_list)
 print(word)
 guess = input("Please enter a single letter.")
-if len(guess) == 1:
+if len(guess) == 1 and guess.isalpha():
     print("Good guess!")
 else:
     print("Oops! That is not a valid input.")
