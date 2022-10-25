c14c515083269906e886646a2b692d2ea3f3274e
mmillet88 <m.kosyuchenko@gmail.com>
2022-10-25 16:40:53 +0100

    Checks an input is a single letter.

--- milestone_2.py
+++ milestone_2.py
@@ -4,3 +4,7 @@ print(word_list)
 word = random.choice(word_list)
 print(word)
 guess = input("Please enter a single letter.")
+if len(guess) == 1:
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
