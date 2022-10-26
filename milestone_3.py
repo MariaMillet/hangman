c8d9b31efe00121268f2158c3162654c556121a9
mmillet88 <m.kosyuchenko@gmail.com>
2022-10-26 10:29:04 +0100

    Creates function to run the checks.

--- milestone_3.py
+++ milestone_3.py
@@ -1,12 +1,7 @@
 import random
 # import milestone_2
 
-while True:
-    guess = input("Please guess a letter")
-    if len(guess) == 1 and guess.isalpha():
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+
 
 # if guess in milestone_2.word:
 #     print(f"Good guess! {guess} is in the word.")
@@ -16,7 +11,22 @@ while True:
 word_list = ['apple', 'banana', 'cherries', "strawberries", "mango"]
 word = random.choice(word_list)
 
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+
+
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+def ask_for_input():
+    while True:
+        guess = input("Please guess a letter")
+        if len(guess) == 1 and guess.isalpha():
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guess)
+
+ask_for_input()
