e19da8a5e23bd295136e7c749279d58040554a75
mmillet88 <m.kosyuchenko@gmail.com>
2022-10-26 13:20:28 +0100

    Creates Hangman class __init__ method inside milestone_4.py.

---
+++ milestone_4.py
@@ -0,0 +1,10 @@
+import random
+
+class Hangman:
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = random(word_list)
+        self.word_guessed = [''] * len(self.word_list)
+        self.num_letters = [1 for i in list(set(self.word)) if i not in self.word_guessed]
+        self.list_of_guesses = []
