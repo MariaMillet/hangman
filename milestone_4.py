c023b7cc763d55d095c035ccaeaaf5777d8ca4f3
mmillet88 <m.kosyuchenko@gmail.com>
2022-10-26 16:48:54 +0100

    A method ask_for_input and guess ask the user for the input and check if guessed correctly, as well as adds the letter to the list of guesses.

--- milestone_4.py
+++ milestone_4.py
@@ -9,5 +9,21 @@ class Hangman:
         self.num_letters = sum([1 for i in list(set(self.word)) if i not in self.word_guessed])
         self.list_of_guesses = []
 
-test = Hangman(['apple'])
-print(test.word_guessed)
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+    
+    def ask_for_input(self):
+        while True:
+            guess = input("Please guess a letter.")
+            if not guess.isalpha() and len(guess) != 1:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.word_guessed:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+
+test = Hangman(['apple', "banana"])
+print(test.ask_for_input())
