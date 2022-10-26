bcec2a21e5721a10eb5d2ec567d339047c43b815
mmillet88 <m.kosyuchenko@gmail.com>
2022-10-26 19:13:29 +0100

    removed list_of_guesses update

--- milestone_4.py
+++ milestone_4.py
@@ -21,7 +21,7 @@ class Hangman:
             self.num_lives -= 1
             print(f"Sorry, {guess} is not in the word.")
             print(f"You have {self.num_lives} lives left.")
-        self.list_of_guesses.append(guess)
+
 
     
     def ask_for_input(self):
