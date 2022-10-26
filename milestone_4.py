1347c0117aa88c48ac4dcda1a10dde534d7c7214
mmillet88 <m.kosyuchenko@gmail.com>
2022-10-26 17:54:23 +0100

    Asks for input corrected for list_of_guesses

--- milestone_4.py
+++ milestone_4.py
@@ -23,7 +23,7 @@ class Hangman:
             guess = input("Please guess a letter.")
             if (not guess.isalpha()) or len(guess) != 1:
                 print("Invalid letter. Please, enter a single alphabetical character.")
-            elif guess in self.word_guessed:
+            elif guess in self.list_of_guesses:
                 print("You already tried that letter!")
             else:
                 self.check_guess(guess)
