5421f2ad853f36fc5c202a49660bef273c46cefa
mmillet88 <m.kosyuchenko@gmail.com>
2022-10-26 19:42:45 +0100

    play_game function defines the game.

---
+++ milestone_5.py
@@ -0,0 +1,51 @@
+import random
+
+class Hangman:
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_'] * len(self.word)
+        self.num_letters = sum([1 for i in list(set(self.word)) if i not in self.word_guessed])
+        self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+            for i in range(len(self.word)):
+                if self.word[i] == guess:
+                    self.word_guessed[i] = guess
+            self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
+
+
+    
+    def ask_for_input(self):
+        while True:
+            guess = input("Please type a letter.")
+            if (not guess.isalpha()) or len(guess) != 1:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.list_of_guesses.append(guess)
+                self.check_guess(guess=guess)
+                break
+
+def play_game(word_list):
+    game = Hangman(word_list, num_lives=3)
+    while True:
+        if game.num_lives == 0:
+            print("You lost!")
+            break
+        elif game.num_letters > 0:
+            game.ask_for_input()
+        else:
+            print("Congratulations. You won the game!")
+            break
+
+play_game(['apple'])
