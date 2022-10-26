c4f8cecbe8920b76702e579d29386c2d9d3a7193
mmillet88 <m.kosyuchenko@gmail.com>
2022-10-26 13:26:19 +0100

    Corrects word_guessed attribute

--- milestone_4.py
+++ milestone_4.py
@@ -5,7 +5,7 @@ class Hangman:
         self.word_list = word_list
         self.num_lives = num_lives
         self.word = random.choice(word_list)
-        self.word_guessed = [''] * len(self.word_list)
+        self.word_guessed = [''] * len(self.word)
         self.num_letters = [1 for i in list(set(self.word)) if i not in self.word_guessed]
         self.list_of_guesses = []
 
